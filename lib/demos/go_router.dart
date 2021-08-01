277766fa05d128bd1cbb786bf30712baa0d25f0d
Chris Sells <csells@sellsbrothers.com>
2021-08-01 15:53:24 -0700

    show the location as it changes

--- lib/demos/go_router.dart
+++ lib/demos/go_router.dart
@@ -1,6 +1,21 @@
 import 'package:flutter/material.dart';
 
-class GoRouterDemo extends StatelessWidget {
+import '../main.dart';
+import 'package:go_router/go_router.dart';
+
+class GoRouterDemo extends StatefulWidget {
+  @override
+  _GoRouterDemoState createState() => _GoRouterDemoState();
+}
+
+class _GoRouterDemoState extends State<GoRouterDemo> {
+  @override
+  void initState() {
+    // hook GoRouter into the urlNotifier that is shared by all the demos
+    urlNotifier.addListener(() => context.go(urlNotifier.value));
+    super.initState();
+  }
+
   @override
   Widget build(BuildContext context) => MaterialApp(
         home: Scaffold(body: Center(child: Text('GoRouterDemo'))),
--- lib/shared_widgets.dart
+++ lib/shared_widgets.dart
@@ -14,7 +14,8 @@ class MainAppScaffold extends StatelessWidget {
   const MainAppScaffold({Key? key, required this.content}) : super(key: key);
   @override
   Widget build(BuildContext context) {
-    EdgeInsets padding = const EdgeInsets.symmetric(vertical: 24, horizontal: 8);
+    EdgeInsets padding =
+        const EdgeInsets.symmetric(vertical: 24, horizontal: 8);
     return Scaffold(
       body: SafeArea(
         child: Column(
@@ -23,10 +24,17 @@ class MainAppScaffold extends StatelessWidget {
             Row(
               children: [
                 _TabBtn('/', label: "Home", padding: padding, exactMatch: true),
-                _TabBtn('/$shirts/$tees', rootPath: shirts, label: "Shirts", padding: padding),
-                _TabBtn('/$pants/$sweats', rootPath: pants, label: "Pants", padding: padding),
-                _TabBtn('/$hats/$toques', rootPath: hats, label: "Hats", padding: padding),
+                _TabBtn('/$shirts/$tees',
+                    rootPath: shirts, label: "Shirts", padding: padding),
+                _TabBtn('/$pants/$sweats',
+                    rootPath: pants, label: "Pants", padding: padding),
+                _TabBtn('/$hats/$toques',
+                    rootPath: hats, label: "Hats", padding: padding),
               ],
+            ),
+            Padding(
+              padding: const EdgeInsets.all(8),
+              child: Text(urlNotifier.value),
             ),
           ],
         ),
@@ -37,12 +45,18 @@ class MainAppScaffold extends StatelessWidget {
 
 class HomeView extends StatelessWidget {
   @override
-  Widget build(BuildContext context) =>
-      Container(alignment: Alignment.center, color: Colors.blue.shade300, child: Text("HOME"));
+  Widget build(BuildContext context) => Container(
+      alignment: Alignment.center,
+      color: Colors.blue.shade300,
+      child: Text("HOME"));
 }
 
 class NestedTabScaffold extends StatelessWidget {
-  const NestedTabScaffold({Key? key, required this.child, required this.category, required this.subCategories})
+  const NestedTabScaffold(
+      {Key? key,
+      required this.child,
+      required this.category,
+      required this.subCategories})
       : super(key: key);
   final Widget child;
   final String category;
@@ -51,20 +65,26 @@ class NestedTabScaffold extends StatelessWidget {
   @override
   Widget build(BuildContext context) => Column(
         children: [
-          Row(children: subCategories.map((e) => _TabBtn('/$category/$e', fontSize: 14)).toList()),
+          Row(
+              children: subCategories
+                  .map((e) => _TabBtn('/$category/$e', fontSize: 14))
+                  .toList()),
           Expanded(child: child),
         ],
       );
 }
 
 class ProductsListPage extends StatelessWidget {
-  ProductsListPage({Key? key, required this.category, required this.subCategory}) : super(key: key);
+  ProductsListPage(
+      {Key? key, required this.category, required this.subCategory})
+      : super(key: key);
 
   final String category;
   final String subCategory;
   late final List<_ProductBtn> _btns = List.generate(
     20,
-    (index) => _ProductBtn("/$category/$subCategory/$productInfo/${Random().nextInt(999)}"),
+    (index) => _ProductBtn(
+        "/$category/$subCategory/$productInfo/${Random().nextInt(999)}"),
   );
 
   @override
@@ -115,7 +135,13 @@ class _ProductBtn extends StatelessWidget {
 }
 
 class _TabBtn extends StatelessWidget {
-  const _TabBtn(this.path, {Key? key, this.label, this.padding, this.rootPath, this.exactMatch = false, this.fontSize})
+  const _TabBtn(this.path,
+      {Key? key,
+      this.label,
+      this.padding,
+      this.rootPath,
+      this.exactMatch = false,
+      this.fontSize})
       : super(key: key);
 
   /// When pressed the button will change urlNotifier.value to this path.
@@ -138,12 +164,15 @@ class _TabBtn extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     String pathToMatch = rootPath ?? path;
-    bool isSelected = exactMatch ? urlNotifier.value == pathToMatch : urlNotifier.value.contains(pathToMatch);
+    bool isSelected = exactMatch
+        ? urlNotifier.value == pathToMatch
+        : urlNotifier.value.contains(pathToMatch);
     Color bgColor = isSelected ? Colors.grey.shade300 : Colors.grey.shade200;
     return Expanded(
       child: OutlinedButton(
           style: ButtonStyle(
-              shape: MaterialStateProperty.all(RoundedRectangleBorder(borderRadius: BorderRadius.zero)),
+              shape: MaterialStateProperty.all(
+                  RoundedRectangleBorder(borderRadius: BorderRadius.zero)),
               backgroundColor: MaterialStateProperty.all(bgColor)),
           onPressed: _handlePressed,
           child: Padding(
@@ -151,7 +180,8 @@ class _TabBtn extends StatelessWidget {
             child: Text(
               label ?? path,
               softWrap: false,
-              style: TextStyle(fontSize: fontSize ?? 20, fontWeight: FontWeight.bold),
+              style: TextStyle(
+                  fontSize: fontSize ?? 20, fontWeight: FontWeight.bold),
               maxLines: 1,
             ),
           )),
@@ -169,7 +199,8 @@ class UnknownPathWidget extends StatelessWidget {
         children: [
           TextButton(
             child: Text('<< Back'),
-            onPressed: () => urlNotifier.value = VRouter.of(context).previousUrl!,
+            onPressed: () =>
+                urlNotifier.value = VRouter.of(context).previousUrl!,
           ),
           Spacer(),
           Text('404: Page not found.'),
