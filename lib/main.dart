9da2a3d8dcdad39526231349c6918858b1d7e3bc
Chris Sells <csells@sellsbrothers.com>
2021-08-11 12:04:37 -0700

    simplified main

--- lib/main.dart
+++ lib/main.dart
@@ -5,29 +5,9 @@ import 'demos/vrouter.dart';
 /// This value notifier is used as the src of truth for all demos
 ValueNotifier<String> urlNotifier = ValueNotifier('/');
 
-enum DemoKind { vrouter, gorouter }
-
 void main(List<String> args) {
-  final demoKind = args.length == 1 && args[0] == 'gorouter'
-      ? DemoKind.gorouter
-      : DemoKind.vrouter;
-
-  runApp(App(demoKind));
-}
-
-class App extends StatelessWidget {
-  final demoKind;
-  const App(this.demoKind, {Key? key}) : super(key: key);
-
-  @override
-  Widget build(BuildContext context) {
-    switch (demoKind) {
-      case DemoKind.vrouter:
-        return VRouterDemo();
-      case DemoKind.gorouter:
-        return GoRouterDemo();
-      default:
-        throw Exception('oops');
-    }
-  }
+  final app = args.length == 1 && args[0] == 'gorouter'
+      ? GoRouterDemo()
+      : VRouterDemo();
+  runApp(app);
 }
